b09cb52a1dc4b8b9061cff61ffd45979926b68ac
dmy3ruk <104530429+dmy3ruk@users.noreply.github.com>
2022-11-12 19:23:17 +0200

    Add files via upload

---
+++ Delete pair num.js
@@ -0,0 +1,17 @@
+const arr=[8, 7, 4, 5, 34, 1];
+let l=arr.length;
+let nmb = "";
+let i;
+for (i=0; i<l; i++){
+     const currentV=arr[i];
+    let p=i;
+    while(p>0 && arr[p-1]>currentV){
+     arr[p]=arr[p-1];
+      p--;
+     }
+    arr[p]=currentV;}
+let a=arr.indexOf(i)
+for (let i=0; i<=l; i++){
+if (arr[i]%2==0){
+  arr[i]=nmb
+}} console.log(arr)
